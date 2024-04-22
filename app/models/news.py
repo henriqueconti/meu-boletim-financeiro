ab0eedd38b0f65a50ab70c129f858dbcd1fded87
henriqueconti <rickteixeira28@gmail.com>
2024-04-21 22:27:51 -0300

    feat cleaning news messages

--- app/models/news.py
+++ app/models/news.py
@@ -4,4 +4,6 @@ from pydantic import BaseModel
 class News(BaseModel):
     title: str
     url: str
+    description: str
+    content: str
     publishedAt: str
--- app/utils/message.py
+++ app/utils/message.py
@@ -23,7 +23,16 @@ def build_message_email(stock_object_list, index_object_list, news_object_list):
 
     message += '\nPrincipais Notícias\n'
 
-    for news_object in news_object_list:
+    for news_object in clear_news_list(news_object_list):
         message += '\n{}\nAcesse: {}\n'.format(news_object.title, news_object.url)
 
     return message, subject
+
+
+def clear_news_list(news_object_list):
+    cleaned_news_object_list = []
+
+    for news_object in news_object_list:
+        if 'Bolsa Família' not in news_object.content:
+            cleaned_news_object_list.append(news_object)
+    return cleaned_news_object_list
